4b34024ad182f4b6a8a72ad54d070aaaa37c03ee
kbenya <kbb1795@gmail.com>
2022-03-08 15:57:56 +0100

    add test tests

--- tasks/if_else/if_else.py
+++ tasks/if_else/if_else.py
@@ -1,3 +1,6 @@
 def if_else(n):
     """Write your code here"""
     pass
+
+
+print(if_else(input()))
---
+++ tests/if-else/test_if_else.py
@@ -0,0 +1,28 @@
+import pytest
+from tasks.if_else.if_else.py import if_else
+
+class IfElse(object):
+
+    def test_1(self):
+        assert if_else(3) == "Weird"
+
+    def test_2(self):
+        assert if_else(24) == "Not Weird"
+
+    def test_3(self):
+        assert if_else(4) == "Not Weird"
+
+    def test_4(self):
+        assert if_else(18) == "Weird"
+
+    def test_5(self):
+        assert if_else(29) == "Weird"
+
+    def test_6(self):
+        assert if_else(5) == "Weird"    
+
+    def test_7(self):
+        assert if_else(100) == "Not Weird"  
+
+    def test_8(self):
+        assert if_else(20) == "Weird"  
