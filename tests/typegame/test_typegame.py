ff4edf4d72e9282638d5b7c155f68309d7cc5c60
Ádám Vig <52835042+adamvig96@users.noreply.github.com>
2022-03-20 12:20:04 +0100

    Update test_typegame.py

--- tests/typegame/test_typegame.py
+++ tests/typegame/test_typegame.py
@@ -40,82 +40,82 @@ class TestTypegame(object):
         assert get_answer(9) == "d"
 
     def test_10(self):
-        assert get_answer(5) == "b"
+        assert get_answer(10) == "b"
 
     def test_11(self):
-        assert get_answer(5) == "c"
+        assert get_answer(11) == "c"
 
     def test_12(self):
-        assert get_answer(5) == "e"
+        assert get_answer(12) == "e"
 
     def test_13(self):
-        assert get_answer(5) == "d"
+        assert get_answer(13) == "d"
 
     def test_14(self):
-        assert get_answer(5) == "d"
+        assert get_answer(14) == "d"
 
     def test_15(self):
-        assert get_answer(5) == "a"
+        assert get_answer(15) == "a"
 
     def test_16(self):
-        assert get_answer(5) == "b"
+        assert get_answer(16) == "b"
 
     def test_17(self):
-        assert get_answer(5) == "a"
+        assert get_answer(17) == "a"
 
     def test_18(self):
-        assert get_answer(5) == "a"
+        assert get_answer(18) == "a"
 
     def test_19(self):
-        assert get_answer(5) == "b"
+        assert get_answer(19) == "b"
 
     def test_20(self):
-        assert get_answer(5) == "c"
+        assert get_answer(20) == "c"
 
     def test_21(self):
-        assert get_answer(5) == "d"
+        assert get_answer(21) == "d"
 
     def test_22(self):
-        assert get_answer(5) == "b"
+        assert get_answer(22) == "b"
 
     def test_23(self):
-        assert get_answer(5) == "c"
+        assert get_answer(23) == "c"
 
     def test_24(self):
-        assert get_answer(5) == "e"
+        assert get_answer(24) == "e"
 
     def test_25(self):
-        assert get_answer(5) == "a"
+        assert get_answer(25) == "a"
 
     def test_26(self):
-        assert get_answer(5) == "e"
+        assert get_answer(26) == "e"
 
     def test_27(self):
-        assert get_answer(5) == "b"
+        assert get_answer(27) == "b"
 
     def test_28(self):
-        assert get_answer(5) == "d"
+        assert get_answer(28) == "d"
 
     def test_29(self):
-        assert get_answer(5) == "e"
+        assert get_answer(29) == "e"
 
     def test_30(self):
-        assert get_answer(5) == "a"
+        assert get_answer(30) == "a"
 
     def test_31(self):
-        assert get_answer(5) == "b"
+        assert get_answer(31) == "b"
 
     def test_32(self):
-        assert get_answer(5) == "d"
+        assert get_answer(32) == "d"
 
     def test_33(self):
-        assert get_answer(5) == "c"
+        assert get_answer(33) == "c"
 
     def test_34(self):
-        assert get_answer(5) == "a"
+        assert get_answer(34) == "a"
 
     def test_35(self):
-        assert get_answer(5) == "c"
+        assert get_answer(35) == "c"
 
     def test_36(self):
-        assert get_answer(5) == "b"
+        assert get_answer(36) == "b"
